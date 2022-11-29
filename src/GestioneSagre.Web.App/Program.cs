1aae8eb99f9e37f3256425db6b1981b30c3d9547
Angelo Pirola <angelo9871@gmail.com>
2022-11-30 00:04:21 +0100

    Recreated two solution projects from new

--- src/GestioneSagre.Web.App/Program.cs
+++ src/GestioneSagre.Web.App/Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using GestioneSagre.Web.App;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace GestioneSagre.Web.App;
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-await builder.Build().RunAsync();
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient
+        {
+            BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+        });
+
+        await builder.Build().RunAsync();
+    }
+}
---
+++ src/GestioneSagre.Web/Startup.cs
@@ -0,0 +1,51 @@
+﻿namespace GestioneSagre.Web;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddRazorPages();
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        if (env.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+
+        var enableSwagger = Configuration.GetSection("Swagger").GetValue<bool>("enabled");
+
+        if (enableSwagger)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre v1");
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseBlazorFrameworkFiles();
+
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+            endpoints.MapControllers();
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
