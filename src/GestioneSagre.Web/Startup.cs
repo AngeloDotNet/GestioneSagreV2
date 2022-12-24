8c1347fa1bbf069f932715d86ab35ef62d4fbefb
Angelo Pirola <angelo9871@gmail.com>
2022-12-24 19:13:21 +0100

    Refactoring

--- src/GestioneSagre.Web/Startup.cs
+++ src/GestioneSagre.Web/Startup.cs
@@ -16,13 +16,24 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("GestioneSagre", policy =>
+            {
+                policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
+
         services.AddOcelot();
+        services.AddEndpointsApiExplorer();
 
         services.AddSwaggerGen(config =>
         {
             config.SwaggerDoc("v1", new OpenApiInfo
             {
-                Title = "Gestione Sagre",
+                Title = "Gestione Sagre Web",
                 Version = "v1"
             });
         });
@@ -32,19 +43,14 @@ public class Startup
     {
         IWebHostEnvironment env = app.Environment;
 
-        var enableSwagger = Configuration.GetSection("Swagger").GetValue<bool>("Enabled");
-
-        if (enableSwagger)
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre v1");
-            });
-        }
-
         app.UseHttpsRedirection();
-        app.UseStaticFiles();
+        app.UseCors("GestioneSagre");
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre v1");
+        });
 
         app.UseRouting();
         app.UseEndpoints(endpoints =>
