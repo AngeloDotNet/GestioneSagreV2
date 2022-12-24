3c98d2182d7bd8e6e56f6f8b42c2e6080028f3c9
Angelo Pirola <angelo9871@gmail.com>
2022-12-24 18:44:31 +0100

    Developed Web API

--- src/GestioneSagre.Utility.Web.Api.Public/Program.cs
+++ src/GestioneSagre.Utility.Web.Api.Public/Program.cs
@@ -1,25 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace GestioneSagre.Utility.Web.Api.Public;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        Startup startup = new(builder.Configuration);
+
+        startup.ConfigureServices(builder.Services);
+
+        WebApplication app = builder.Build();
+
+        startup.Configure(app);
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ src/GestioneSagre.Utility.Web.Api.Public/Startup.cs
@@ -0,0 +1,77 @@
+﻿using GestioneSagre.Utility.Business;
+using GestioneSagre.Utility.Business.Handlers.Read;
+using GestioneSagre.Utility.Domain.Mapping;
+using GestioneSagre.Utility.Infrastructure.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace GestioneSagre.Utility.Web.Api.Public;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("UtilityPublicApi", policy =>
+            {
+                policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(config =>
+        {
+            config.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Gestione Sagre public API - Utility",
+                Version = "v1"
+            });
+        });
+
+        var connectionString = Configuration.GetSection("ConnectionStrings").GetValue<string>("Default");
+
+        services.AddDbContext<DataDbContext>(options =>
+            options.UseSqlite(connectionString, migration =>
+                migration.MigrationsAssembly(typeof(DataDbContext).Assembly.FullName))
+        );
+
+        services.AddApplicationCore();
+        services.AddInfrastructure();
+
+        services.AddAutoMapper(typeof(EmailMessageMapperProfile));
+        services.AddMediatR(typeof(GetEmailMessagesListHandler).Assembly);
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        app.UseHttpsRedirection();
+        app.UseCors("UtilityPublicApi");
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre public API V1 - Utility");
+        });
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
