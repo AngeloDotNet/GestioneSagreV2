a8a61dbb34b4ee67d07a85a2d5cb3357b041ce7e
Angelo Pirola <angelo9871@gmail.com>
2022-12-24 18:43:53 +0100

    Library developed

---
+++ src/GestioneSagre.Utility.Domain/Models/InputModels/DeleteEmailMessageInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GestioneSagre.Utility.Domain.Models.InputModels;
+
+public class DeleteEmailMessageInputModel
+{
+    public int Id { get; set; }
+    public Guid EmailId { get; set; }
+
+    public DeleteEmailMessageInputModel(int id, Guid emailId)
+    {
+        Id = id;
+        EmailId = emailId;
+    }
+}
---
+++ src/GestioneSagre.Utility.Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using GestioneSagre.Utility.Infrastructure.Repository.Read;
+using GestioneSagre.Utility.Infrastructure.Repository.Write;
+
+namespace GestioneSagre.Utility.Domain.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUtilityReadRepository UtilityRead { get; }
+    IUtilityWriteRepository UtilityWrite { get; }
+}
