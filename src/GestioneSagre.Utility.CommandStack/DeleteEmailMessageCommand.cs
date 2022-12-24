0a6669e8c1a314187018720094c8bbc7a7d1aa1b
Angelo Pirola <angelo9871@gmail.com>
2022-12-24 18:43:48 +0100

    Library developed

---
+++ src/GestioneSagre.Utility.CommandStack/DeleteEmailMessageCommand.cs
@@ -0,0 +1,15 @@
+﻿using GestioneSagre.Utility.Domain.Models.InputModels;
+using MediatR;
+
+namespace GestioneSagre.Utility.CommandStack;
+public class DeleteEmailMessageCommand : IRequest<bool>
+{
+    public int Id { get; set; }
+    public Guid EmailId { get; set; }
+
+    public DeleteEmailMessageCommand(DeleteEmailMessageInputModel inputModel)
+    {
+        Id = inputModel.Id;
+        EmailId = inputModel.EmailId;
+    }
+}
---
+++ src/GestioneSagre.Utility.CommandStack/UpdateEmailMessageCommand.cs
@@ -0,0 +1,32 @@
+﻿using GestioneSagre.Utility.Domain.Models.InputModels;
+using GestioneSagre.Utility.Infrastructure.Enum;
+using MediatR;
+
+namespace GestioneSagre.Utility.CommandStack;
+public class UpdateEmailMessageCommand : IRequest<bool>
+{
+    public int Id { get; set; }
+    public Guid EmailId { get; set; }
+    public string Recipient { get; set; }
+    public string RecipientEmail { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+    public DateTime SendDate { get; set; }
+    public DateTime EffectiveSendDate { get; set; }
+    public int EmailSendCount { get; set; }
+    public EmailStatus Status { get; set; }
+
+    public UpdateEmailMessageCommand(UpdateEmailMessageInputModel inputModel)
+    {
+        Id = inputModel.Id;
+        EmailId = inputModel.EmailId;
+        Recipient = inputModel.Recipient;
+        RecipientEmail = inputModel.RecipientEmail;
+        Subject = inputModel.Subject;
+        Message = inputModel.Message;
+        SendDate = inputModel.SendDate;
+        EffectiveSendDate = inputModel.EffectiveSendDate;
+        EmailSendCount = inputModel.EmailSendCount;
+        Status = inputModel.Status;
+    }
+}
