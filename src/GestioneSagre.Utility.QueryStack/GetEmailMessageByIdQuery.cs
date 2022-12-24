8cfc09d06a052be088892dd60574e88f2bfaf7cc
Angelo Pirola <angelo9871@gmail.com>
2022-12-24 18:43:35 +0100

    Library developed

---
+++ src/GestioneSagre.Utility.QueryStack/GetEmailMessageByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using GestioneSagre.Utility.Domain.Models.ViewModels;
+using MediatR;
+
+namespace GestioneSagre.Utility.QueryStack;
+
+public class GetEmailMessageByIdQuery : IRequest<EmailMessageViewModel>
+{
+    public int Id { get; set; }
+
+    public GetEmailMessageByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
