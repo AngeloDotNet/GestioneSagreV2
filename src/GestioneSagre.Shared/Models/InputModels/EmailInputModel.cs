a54ca1b0c52876a779a42472ad0f75d6c19fa2f8
Angelo Pirola <angelo9871@gmail.com>
2022-12-26 20:38:33 +0100

    Refactoring

---
+++ src/GestioneSagre.Shared/Models/InputModels/EmailInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GestioneSagre.Shared.Models.InputModels;
+
+public class EmailInputModel
+{
+    public string RecipientEmail { get; set; }
+    public string ReplyEmail { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+}
