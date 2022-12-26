2bf906498a89fb0499e50b5c09cdd5b12017b7c1
Angelo Pirola <angelo9871@gmail.com>
2022-12-26 20:39:15 +0100

    Finished library development

--- src/GestioneSagre.Utility.WorkerServices/Receivers/EmailMessageReceiver.cs
+++ src/GestioneSagre.Utility.WorkerServices/Receivers/EmailMessageReceiver.cs
@@ -1,4 +1,7 @@
-﻿using GestioneSagre.RabbitMQ.Abstractions;
+﻿using System.Net;
+using System.Net.Http.Json;
+using GestioneSagre.RabbitMQ.Abstractions;
+using GestioneSagre.Shared.Models.InputModels;
 using GestioneSagre.Utility.Domain.Models.InputModels;
 
 namespace GestioneSagre.Utility.WorkerServices.Receivers;
@@ -6,10 +9,12 @@ namespace GestioneSagre.Utility.WorkerServices.Receivers;
 public class EmailMessageReceiver : IMessageReceiver<CreateEmailMessageInputModel>
 {
     private readonly ILogger logger;
+    private readonly HttpClient httpClient;
 
-    public EmailMessageReceiver(ILogger<EmailMessageReceiver> logger)
+    public EmailMessageReceiver(ILogger<EmailMessageReceiver> logger, HttpClient httpClient)
     {
         this.logger = logger;
+        this.httpClient = httpClient;
     }
 
     public async Task ReceiveAsync(CreateEmailMessageInputModel message, CancellationToken cancellationToken)
@@ -17,13 +22,23 @@ public class EmailMessageReceiver : IMessageReceiver<CreateEmailMessageInputMode
         logger.LogInformation("Send the email to {Recipient} - {RecipientEmail}, with subject {Subject} and message {Message}",
             message.Recipient, message.RecipientEmail, message.Subject, message.Message);
 
-        //Invio l'email ed attendo l'esito dell'invio
-        await Task.Delay(TimeSpan.FromSeconds(10 + Random.Shared.Next(10)), cancellationToken);
+        EmailInputModel emailMessage = new()
+        {
+            RecipientEmail = message.RecipientEmail,
+            ReplyEmail = null,
+            Subject = message.Subject,
+            Message = message.Message
+        };
+
+        var result = await httpClient.PostAsJsonAsync("https://localhost:7228/api/email", emailMessage, cancellationToken);
+
+        if (result.StatusCode != HttpStatusCode.OK)
+        {
+            //Se esito negativo aggiorno lo stato della mail a Failed
+            logger.LogWarning("The sending of the email {EmailId}, failed.", message.EmailId);
+        }
 
         //Se esito positivo aggiorno lo stato della mail a Sent
         logger.LogInformation("Sending the email {EmailId}, has been sent successfully", message.EmailId);
-
-        //Se esito negativo aggiorno lo stato della mail a Failed
-        logger.LogWarning("The sending of the email {EmailId}, failed.", message.EmailId);
     }
 }
