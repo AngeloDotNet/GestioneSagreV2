8c0b25512b1869883c954b1615b58a6f0915311d
Angelo Pirola <angelo9871@gmail.com>
2022-12-27 13:35:28 +0100

    Updated Ocelot configuration

--- src/GestioneSagre.Web/Program.cs
+++ src/GestioneSagre.Web/Program.cs
@@ -5,7 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-        builder.Configuration.AddJsonFile("ocelot.json");
+        builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
         Startup startup = new(builder.Configuration);
 
