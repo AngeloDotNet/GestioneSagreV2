33ff0048ebc6e227522cc7dec4b603c1414e60e6
Angelo Pirola <angelo9871@gmail.com>
2022-12-27 19:58:00 +0100

    Code refactoring and cleaning

--- src/GestioneSagre.Utility.Web.Api.Internal/HostedServices/EmailSenderHostedService.cs
+++ src/GestioneSagre.Utility.Web.Api.Internal/HostedServices/EmailSenderHostedService.cs
@@ -1,7 +1,7 @@
 ﻿using GestioneSagre.Shared.RabbitMQ.InputModels;
 using GestioneSagre.Tools.MailKit.Options;
 using GestioneSagre.Tools.RabbitMQ.Abstractions;
-using GestioneSagre.Utility.Web.Api.Internal.Services;
+using GestioneSagre.Utility.Core;
 using Microsoft.Extensions.Options;
 
 namespace GestioneSagre.Utility.Web.Api.Internal.HostedServices;
