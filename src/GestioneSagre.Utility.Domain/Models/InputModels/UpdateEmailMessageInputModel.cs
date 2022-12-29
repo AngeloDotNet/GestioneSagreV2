8d856d8a7ddc26a9e0e054f61720470864a15961
Angelo Pirola <angelo9871@gmail.com>
2022-12-29 15:08:36 +0100

    Input models validation added

--- src/GestioneSagre.Utility.Domain/Models/InputModels/UpdateEmailMessageInputModel.cs
+++ src/GestioneSagre.Utility.Domain/Models/InputModels/UpdateEmailMessageInputModel.cs
@@ -1,19 +1,20 @@
-﻿using GestioneSagre.Shared.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using GestioneSagre.Shared.Entities;
 using GestioneSagre.Utility.Infrastructure.Enum;
 
 namespace GestioneSagre.Utility.Domain.Models.InputModels;
 
 public partial class UpdateEmailMessageInputModel : BaseEntity
 {
-    public Guid EmailId { get; set; }
-    public string Recipient { get; set; }
-    public string RecipientEmail { get; set; }
-    public string Subject { get; set; }
-    public string Message { get; set; }
-    public DateTime SendDate { get; set; }
-    public DateTime EffectiveSendDate { get; set; }
-    public int EmailSendCount { get; set; }
-    public EmailStatus Status { get; set; }
+    [Required(ErrorMessage = "EmailId is required")] public Guid EmailId { get; set; }
+    [Required(ErrorMessage = "Recipient is required")] public string Recipient { get; set; }
+    [Required(ErrorMessage = "RecipientEmail is required")] public string RecipientEmail { get; set; }
+    [Required(ErrorMessage = "Subject is required")] public string Subject { get; set; }
+    [Required(ErrorMessage = "Message is required")] public string Message { get; set; }
+    [Required(ErrorMessage = "SendDate is required")] public DateTime SendDate { get; set; }
+    [Required(ErrorMessage = "EffectiveSendDate is required")] public DateTime EffectiveSendDate { get; set; }
+    [Required(ErrorMessage = "EmailSendCount is required")] public int EmailSendCount { get; set; }
+    [Required(ErrorMessage = "Status is required")] public EmailStatus Status { get; set; }
 
     public UpdateEmailMessageInputModel(int id, Guid emailId, string recipient, string recipientEmail, string subject,
         string message, DateTime sendDate, DateTime effectiveSendDate, int emailSendCount, EmailStatus status)
